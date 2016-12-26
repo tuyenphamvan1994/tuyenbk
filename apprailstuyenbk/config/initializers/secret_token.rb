9fae36e17b33d704ec6c75819a0c4174a2f99524
Pham Van Tuyen <tuyenphamvan1994@gmail.com>
2016-12-26 16:16:06 +0700

    Firt Commit

---
+++ apprailstuyenbk/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apprailstuyenbk::Application.config.secret_key_base = '5d19c67fbef40ecbe4c952e3ff01e5b4fd1a325b42d42bc899c76465e2c99dd05b98037c71173b9ebd5b68b1ef58ca5cf48b298aeacc842db48ac3ce0276c597'
